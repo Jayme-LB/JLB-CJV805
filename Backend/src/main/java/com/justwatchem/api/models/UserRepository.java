a5e321211c2c9d308c86a798fa65a5a28736a887
Jayme-LB <Jayme.LasoB@gmail.com>
2025-04-15 16:46:00 -0400

    Removed placeholder method.

--- Backend/src/main/java/com/justwatchem/api/models/UserRepository.java
+++ Backend/src/main/java/com/justwatchem/api/models/UserRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 // This interface will handle CRUD operations with MongoDB for the users collection.
 @Repository
 public interface UserRepository extends MongoRepository<Users, String> {
-  // This method needs to be implemented.
-  //Users findByUserName(String username);
+  
 }
